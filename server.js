f8ca484bb8e7a8ee221ad389a8b2590c70828c96
FISmart1 <yusufftibazma@gmail.com>
2026-05-25 10:42:00 +0700

    push awal

---
+++ server.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "https://lepasjuang.smktibazma.sch.id",
+      "http://localhost:3000",
+      "http://localhost:3001" // untuk development React Vite
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+app.use(express.json());
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/api/wishes", async (req, res) => {
+  try {
+    const [rows] = await db.query(
+      "SELECT * FROM wishes ORDER BY id DESC"
+    );
+
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: "Gagal mengambil data" });
+  }
+});
+
+app.post("/api/wishes", async (req, res) => {
+  try {
+    const { name, major, message } = req.body;
+
+    if (!name || !message) {
+      return res.status(400).json({
+        message: "Nama dan pesan wajib diisi",
+      });
+    }
+
+    const initial = name.trim().charAt(0).toUpperCase();
+
+    const [result] = await db.query(
+      `INSERT INTO wishes (name, major, message, initial)
+       VALUES (?, ?, ?, ?)`,
+      [
+        name,
+        major || "Alumni / Tamu",
+        message,
+        initial,
+      ]
+    );
+
+    const [newWish] = await db.query(
+      "SELECT * FROM wishes WHERE id = ?",
+      [result.insertId]
+    );
+
+    res.status(201).json(newWish[0]);
+  } catch (error) {
+    res.status(500).json({ message: "Gagal menyimpan ucapan" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
